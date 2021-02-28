d970a5ca4757e0b73052020da99d59a72d6e813c
avi-ac <79776492+avi-ac@users.noreply.github.com>
2021-02-27 20:16:31 -0700

    Update GitDemo.java
    I added my name, added the previousNumber variable to GitDemo class, added   reverseNum = newNumber; to the reverseNum() function.

--- GitDemo.java
+++ GitDemo.java
@@ -1,12 +1,14 @@
 /*****
- * Edit this line to have your name.
+ * Avinash Achari
  ******/
 public class GitDemo {
     
     private double previousNumber;
+    private double reverseNum; 
     
     public GitDemo() {
         previousNumber = 1;
+        
     }
     
     /*****
@@ -14,7 +16,8 @@ public class GitDemo {
     *****/
     public double addNumber(double num) {
         System.out.print("Add num to previousNumber");
-        return 0;
+        double ret = num + previousNumber;
+        return ret;
     }
     
     /*****
@@ -41,11 +44,12 @@ public class GitDemo {
      *****/
     public double reverseNumber(int num){
         double newNumber = 1;
+
         
         for(int i = 1; i <= num; ++i){
             newNumber = newNumber * i;
         }
-        
+        reverseNum = newNumber; //added this line
         return newNumber;
     }
     
